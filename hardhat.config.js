28dd1794b224e493247f15379ebf328fcffef1b3
BeauC2481 <91040365+BeauC2481@users.noreply.github.com>
2022-09-21 13:54:30 +1000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,11 @@ require('@nomiclabs/hardhat-waffle');
 const privateKey = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
+  solidity: '0.8.0',
   networks: {
-    hardhat: {
-      chainId: 31337,
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/UNfhfGfQ9lrSjTGUS_X-BM-G0_lMEVh9',
+      accounts: ['b12b2eba25490d6566b12bbf626d68845c297a9bc1216a7d753dc2e9528ab464'],
     },
   },
   solidity: '0.8.4',
