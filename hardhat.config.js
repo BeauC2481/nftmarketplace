707268ab0649699a89f666b9ffaa390c442c375b
BeauC2481 <91040365+BeauC2481@users.noreply.github.com>
2022-09-22 08:29:42 +1000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,13 @@
-const fs = require('fs');
-require('@nomiclabs/hardhat-waffle');
-
-const privateKey = fs.readFileSync('.secret').toString().trim();
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
-  solidity: '0.8.4',
+  solidity: "0.8.9",
   networks: {
     goerli: {
       url: 'https://eth-goerli.alchemyapi.io/v2/UNfhfGfQ9lrSjTGUS_X-BM-G0_lMEVh9',
-      accounts: [privateKey],
+      accounts: ['b12b2eba25490d6566b12bbf626d68845c297a9bc1216a7d753dc2e9528ab464'],
     },
   },
 };
-
 
