4b3894fe5bc8a77d10120084c3e6ca50c5164667
BeauC2481 <91040365+BeauC2481@users.noreply.github.com>
2022-09-21 13:15:02 +1000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/UNfhfGfQ9lrSjTGUS_X-BM-G0_lMEVh9',
-      accounts: [b12b2eba25490d6566b12bbf626d68845c297a9bc1216a7d753dc2e9528ab464],
+      accounts: ['b12b2eba25490d6566b12bbf626d68845c297a9bc1216a7d753dc2e9528ab464'],
     },
   },
   solidity: '0.8.4',
