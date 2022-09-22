e8383cc34718a00ba8641aa849aad5707fdcaed2
BeauC2481 <91040365+BeauC2481@users.noreply.github.com>
2022-09-22 14:05:00 +1000

    Add files via upload

---
+++ context/constants.js
@@ -0,0 +1,6 @@
+import market from './contractsData/nftmarketplace.json';
+import marketaddress from './contractsData/nftmarketplace-address.json';
+
+export const MarketAddress = marketaddress.address;
+export const MarketAddressABI = market.abi;
+
